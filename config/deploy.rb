818d054f781891a74e981500b0298737047fa55f
Freika <iamfrey@gmail.com>
2015-12-10 17:32:26 +0600

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,6 +12,10 @@ set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public
 # set :whenever_command, 'RAILS_ENV=production rvm use 2.2.0 do bundle exec whenever'
 # set :whenever_environment, defer { rails_env }
 
+set :crono_pid, -> { File.join(shared_path, 'tmp', 'pids', 'crono.pid') }
+set :crono_env, -> { fetch(:rack_env, fetch(:rails_env, fetch(:stage))) }
+set :crono_log, -> { File.join(shared_path, 'log', 'crono.log') }
+set :crono_role, -> { :app }
 
 namespace :deploy do
 
