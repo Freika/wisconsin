e7497eebe22661e13c516b6e3e2486860156b0e0
Freika <iamfrey@gmail.com>
2015-12-27 04:06:29 +0600

    asd

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,12 +6,12 @@ set :deploy_to, '/home/deploy/wisconsin'
 set :linked_files, %w{config/database.yml config/secrets.yml config/application.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
-# set :whenever_environment, defer { stage }
+require 'whenever/capistrano'
+set :whenever_environment, defer { stage }
 # set :whenever_identifier, defer { "#{application}_#{stage}" }
 set :whenever_identifier, ->{ "#{fetch(:application)}_#{fetch(:stage)}" }
 set :whenever_command, "bundle exec whenever"
 set :whenever_variables, defer { "'environment=#{rails_env}&current_path=#{current_path}'" }
-require 'whenever/capistrano'
 
 
 # set :crono_pid, -> { File.join(shared_path, 'tmp', 'pids', 'crono.pid') }
